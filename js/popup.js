9bc4c02b6f6f1b6589c5f77422fc02342ff6cdc8
ramanverma <rverma7k@gmail.com>
2020-01-19 03:05:39 +0530

    structure change

---
+++ js/popup.js
@@ -0,0 +1,26 @@
+var counter = 0;
+var timeleft = 60;
+
+function convertSeconds(s) {
+  var min = floor(s / 60);
+  var sec = floor(s % 60);
+  return nf(min, 2) + ":" + nf(sec, 2);
+}
+
+function setup() {
+  noCanvas();
+
+  var timer = select("#timer");
+  timer.html(convertSeconds(timeleft - counter));
+
+  var interval = setInterval(timeIt, 1000);
+
+  function timeIt() {
+    counter++;
+    timer.html(convertSeconds(timeleft - counter));
+    if (counter == timeleft) {
+      clearInterval(interval);
+      //counter = 0;
+    }
+  }
+}
